bf99112c1361a068a02f663e76f9946901184c74
janhnhi <janhnhi@gmail.com>
2023-08-01 09:56:53 -0400

    Removed content.js

--- jane.test/background.js
+++ jane.test/background.js
@@ -1,16 +1,16 @@
 console.log('inside background.js');
 
-// Example of a simple user data object
-const user = {
-    username: 'demo-user'
-};
+// // Example of a simple user data object
+// const user = {
+//     username: 'demo-user'
+// };
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-// 2. A page requested user data, respond with a copy of `user`
-    if (message === 'get-user-data') {
-        sendResponse(user);
-    }
-});
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+// // 2. A page requested user data, respond with a copy of `user`
+//     if (message === 'get-user-data') {
+//         sendResponse(user);
+//     }
+// });
 
 // chrome.runtime.onConnect.addListener(function (port) {
 //     if (port.name == "devtools-page") {
--- jane.test/scripts/content.js
+++
@@ -1,7 +0,0 @@
-console.log('inside content');
-
-chrome.runtime.sendMessage('get-user-data', (response) => {
-    // 3. Got an asynchronous response with the data from the service worker
-    console.log('received user data', response);
-    // initializeUI(response);
-  });
